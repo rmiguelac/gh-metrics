9c3ce9373f93d1d0f822974e3ae3bd8c9888631c
Rui Constancio <r.miguel.a.c@gmail.com>
2023-09-14 08:45:16 -0300

    WIP: configuration

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -1,6 +1,38 @@
 package configuration
 
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
 type Configuration struct {
 	Organization string
 	Repository   string
+}
+
+func New() *Configuration {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	var c Configuration
+	viper.SetDefault(c.Organization, os.Getenv("GH_ORGANIZATION"))
+	viper.SetDefault(c.Repository, os.Getenv("GH_REPOSITORY"))
+	err := viper.Unmarshal(&c)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+
+	log.Fatalf("Reading variables...")
+	log.Fatalf("Organization is %s", c.Organization)
+	log.Fatalf("Repository is %s", c.Repository)
+
+	return &c
 }
